03010052119d3d0fa066f1842375ecebec4b3c7b
quieroserpm <quieroserproductmanager@gmail.com>
2021-08-29 19:25:24 -0300

    commit full projeto

--- src/controllers/CandidateController.js
+++ src/controllers/CandidateController.js
@@ -4,19 +4,32 @@ const Candidate = require('../models/Candidate');
 module.exports = {
     async register(req, res) {
 
-        const { email, gender, name, cep } = req.body;
+        const { name, cargo, datanas, estadoc, gender, cep, logradouro, numero, bairro, 
+            localidade, uf, email, identidade, cpf, vehiculo, habilitacao } = req.body;
 
         const newCandidate = new Candidate();
 
-        newCandidate.email = email;
         newCandidate.name = name;
+        newCandidate.cargo = cargo;
+        newCandidate.datanas = datanas;
+        newCandidate.estadoc = estadoc;
         newCandidate.gender = gender;
         newCandidate.cep = cep;
+        newCandidate.logradouro = logradouro;
+        newCandidate.numero = numero;
+        newCandidate.bairro = bairro;
+        newCandidate.localidade = localidade;
+        newCandidate.uf = uf;
+        newCandidate.email = email;
+        newCandidate.identidade = identidade;
+        newCandidate.cpf = cpf;
+        newCandidate.vehiculo = vehiculo;
+        newCandidate.habilitacao = habilitacao;
 
         newCandidate.save((err, savedCandidate) => {
             if (err) {
                 console.log(err);
-                return res.status(500).send('iti malia deu probleminha!');
+                return res.status(500).send('Erro! Tente novamente!');
             }
 
             return res.status(200).send(savedCandidate);
--- src/models/Candidate.js
+++ src/models/Candidate.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 
 const CandidateSchema = new mongoose.Schema({
     name: { type: String, unique: false, required: true },
-    email: { type: String, unique: true, required: true },
-    gender: { type: String, unique: false, required: true },
-    cep: { type: String, unique: false, required: true }
+    cargo: { type: String, unique: false, required: false },
+    datanas: { type: String, unique: false, required: true },
+    estadoc: { type: String, unique: false, required: false },
+    gender: { type: String, unique: false, required: false },
+    cep: { type: String, unique: false, required: true }, 
+    logradouro: { type: String, unique: false, required: true }, 
+    numero: { type: String, unique: false, required: true }, 
+    bairro: { type: String, unique: false, required: true }, 
+    localidade: { type: String, unique: false, required: true }, 
+    uf: { type: String, unique: false, required: true }, 
+    email: { type: String, unique: false, required: true },
+    identidade: { type: String, unique: false, required: true },
+    cpf: { type: String, unique: true, required: true },
+    vehiculo: { type: String, unique: false, required: false }, 
+    habilitacao: { type: String, unique: false, required: false }
 }, {
     timestamps: true
 });
